0029c11b981e982d5c59f044d03f55c91ef27f96
Zubair12345678910 <zubairsheihh67@gmail.com>
2023-10-08 06:16:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+alert("error! Please enter a valid password");
+
+alert("Wellcome to js land...Happy Coding");
+
+alert("Wellcome to js land");
+
+alert("Hello.. I can run JS through my web browser's console");
